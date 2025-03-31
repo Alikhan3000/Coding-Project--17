d44f2aaa61b366b1ab9abd7ffcb559f5d6be48d4
Alikhan <fixator2004@gmail.com>
2025-03-30 20:27:32 -0400

    Task 2: Created a SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -20,6 +20,7 @@ getTotalSpent() {                       //created a method that returns total am
 
 //created a variable for the function 
 let customer = new Customer("Grace Hopper", "gracehr@gmail.com")
+
 customer.addPurchase(120);              //added some $140 for the total amount spent 
 customer.addPurchase(20);      
 
@@ -28,3 +29,31 @@ console.log(`Customer name: ${customer.name}`);
 console.log(`Customer email: ${customer.email}`);
 console.log(`Total amount spent: $${customer.getTotalSpent()}`);
 
+//Task 2: Create a SalesRep Class
+
+class SalesRep {                        //created a class with 2 properties (1 string, 1 array) 
+    constructor (name, clients){
+        this.name = name;
+        this.clients = [];
+    }
+
+addClient(customer) {                   //created a function to add clients to the array 
+    this.clients.push(customer);
+}
+
+getClientTotal(name) {                  //created a function that returns client's total spending by name
+    for (let client of this.clients){   
+        if (client.name === name){
+            return client.getTotalSpent();
+        }}
+    
+            return `Customer ${name} does not exits`    //returns an error if customer name is not found
+}}
+
+//created a test variable
+let salesRepresentative1 = new SalesRep("Alikhan Nasyrbekov");
+salesRepresentative1.addClient(customer);
+//logged sales reps details, client database, and representative's clients' total spending 
+console.log(`${salesRepresentative1.name} - Sales Representative`);
+console.log(`${salesRepresentative1.name}'s Existing Client Database: ${salesRepresentative1.clients.map(c => c.name).toString()}`);
+console.log(`${salesRepresentative1.name}'s total client spending: $${salesRepresentative1.getClientTotal("Grace Hopper")}`);
